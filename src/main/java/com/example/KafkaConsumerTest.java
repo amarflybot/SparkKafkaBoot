957a3c9f38aeaf7f0e12b904040b3ca73472dd29
Amarendra Kumar <amar.arawat@gmail.com>
2017-03-17 21:00:51 +0530

    First working copy

--- src/main/java/com/example/KafkaConsumerTest.java
+++ src/main/java/com/example/KafkaConsumerTest.java
@@ -11,15 +11,12 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
-import scala.Boolean;
-import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.Collection;
